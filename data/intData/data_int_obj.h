cc284092ccd895ebf9fcbf34c052270fea893bbd
jonathanreno <jonathanreno30@gmail.com>
2023-02-12 20:10:09 -0500

    init

---
+++ data/intData/data_int_obj.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Data_obj 
+{
+private:
+unsigned int my_u_int; 
+signed int my_s_int;
+
+public:
+void set_uint_data(unsigned int d);
+void show_uint_data(); 
+};//member function to display data
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "data/testData/test.h"
+
+int main()
+{
+    Test test1;
+    test1.set_test_int(5);
+    test1.show_test_int();
+
+    return 0;
+};
