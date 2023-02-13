0a809b48d447fd9a5e760362204e1c1e94a60e50
jonathanreno <jonathanreno30@gmail.com>
2023-02-12 20:55:36 -0500

    int data

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include "data/testData/test.h"
+#include "data/intData/data_int_obj.h"
 
 int main()
 {
-    Test test1;
-    test1.set_test_int(5);
-    test1.show_test_int();
+    Int_obj test1;
+    test1.set_uint_data(5);
+    test1.show_uint_data();
+    test1.set_sint_data(-5);
+    test1.show_sint_data();
+    test1.set_ulint_data(10);
+    test1.show_ulint_data();
+    test1.set_slint_data(-10);
+    test1.show_slint_data();
 
     return 0;
-};
+};//member function to display data
