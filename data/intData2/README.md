a07f8378fce9f8c869315d8a37a3fc748b915414
jonathanreno <jonathanreno30@gmail.com>
2023-02-15 17:53:28 -0500

    constructors

---
+++ data/intData2/README.md
@@ -0,0 +1,8 @@
+# 1
+- notice that when running the methods in this file, they all output the integer '0' even though different numbers are passed to the methods in the main function. 
+
+- that members initialized in the initializer list are given a value (0) before the constructor even starts to execute.
+
+# 2
+- notice the empty body of the constructor
+- Actions more complicated than simple initialization must be carried out in the constructor body, as with ordinary functions.
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "data/intData1/data_int_obj.h"
 #include "data/intData2/data_int_obj_2.h"
+#include "data/intData3/data_int_obj_3.h"
 
 int main()
 {
     Int_obj test1;
     Int_obj_2 test2;
+    Int_obj_3 test3;
 
     test1.set_uint_data(5);
     test1.show_uint_data();
@@ -20,11 +22,12 @@ int main()
     test1.set_ssint_data(-20);
     test1.show_ssint_data();
 
-    test2.show_uint_data(10);
-    test2.show_sint_data(-10);
-    test2.show_ulint_data(15);
-    test2.show_slint_data(-15);
-    test2.show_usint_data(20);
+    test3.show_uint_data(10);
+    test3.show_sint_data(-10);
+    test3.show_ulint_data(15);
+    test3.show_slint_data(-15);
+    test3.show_usint_data(20);
+    test3.show_ssint_data(-25);
 
     return 0;
 };//member function to display data
